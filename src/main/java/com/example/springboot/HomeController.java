cc683b25db04f54384d6bda84fcb124de5ffb776
Ramakrishna199828 <rknayudu1@gmail.com>
2025-04-28 16:23:40 +0530

    changes to springboot app

--- src/main/java/com/example/springboot/HomeController.java
+++ src/main/java/com/example/springboot/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello, World! from springboot Application hjkdshjdhfk";
+        return "Hello, World! from springboot Application by Ramakrishna";
     }
 }
